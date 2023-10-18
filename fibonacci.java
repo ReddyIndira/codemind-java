2bc616b250bb191607c1bf7f01b499435c063e5f
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2023-10-18 15:23:24 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,30 @@
+/*import java.util.*;
+class Fibonacci
+{
+           public static void main(String args[])
+	{
+	         int a,b,c,n;
+	         Scanner sc=new Scanner(System.in);
+                     n=sc.nextInt();
+                    int i=1;
+                     a=0;
+                     b=1;
+                     while(i<=n)
+                     {
+                             System.out.print(a);
+                              c=a+b;
+                              a=b;
+                              b=c;
+                              i++;
+                      }
+                       
+             }
+}*/
+
+class m1{
+        public static void main(String args[]){
+                System.out.println("hieeee");
+        }
+}
+
+                     
